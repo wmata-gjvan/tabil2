46637436896d076fcc90431d1536e3bdf77c862e
sgm8 <112197137+sgm8@users.noreply.github.com>
2023-08-07 12:00:54 -0400

    added variables to app.py

--- app.py
+++ app.py
@@ -31,7 +31,15 @@ def config():
     stats = dict(station = request.args.get('station'),
     bufferTime = request.args.get('buffertime'),
     arrivalCountdown = request.args.get('arrcount'),
-    platformCountdown = request.args.get('pltcount'))
+    platformCountdown = request.args.get('pltcount'),
+    weekdaystart1 = request.args.get('weekdaystart1'),
+    weekdayend1 = request.args.get('weekdayend1'),
+    weekdaystart2 = request.args.get('weekdaystart2'),
+    weekdayend2 = request.args.get('weekdayend2'),
+    weekendstart1 = request.args.get('weekendstart1'),
+    weekendend1 = request.args.get('weekendend1'),
+    weekendstart2 = request.args.get('weekendstart2'),
+    weekendend2 = request.args.get('weekendend2'))
     cfgfile = open("config.json", 'w')
     cfgfile.write(json.dumps(stats))
     return "success"
